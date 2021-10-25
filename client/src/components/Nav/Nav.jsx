6ca2c596ed7f64738768a9a9f6bc2e662b66931e
Sean Hughes <sneakerheadsean@gmail.com>
2021-10-25 18:35:26 -0400

    Update Nav.jsx

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
     return (
         <nav>
             <div className="nav">
-                <NavLink className="logo" to="/">ProductsApp</NavLink>
+                <NavLink className="logo" to="/">The Gallery</NavLink>
                 <div className="links">
                     <NavLink className="link" to="/products">Products</NavLink>
                     <NavLink className="link" to="/add-product">Add Product</NavLink>
