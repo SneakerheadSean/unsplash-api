cdca979546928fad0f561fd1df99e6c182772c5c
Sean Hughes <sneakerheadsean@gmail.com>
2021-10-26 09:06:17 -0400

    Update apiConfig.js

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let apiUrl
 
 const apiUrls = {
-    production: 'https://sei-products-api.herokuapp.com/api',
+    production: 'https://salty-everglades-19669.herokuapp.com/api/products',
     development: 'http://localhost:3000/api'
 }
 
